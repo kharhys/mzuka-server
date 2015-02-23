b241b551a89f7a0684d0930b5dd53411b3ebf9f0
kharhys <kelgitonga@gmail.com>
2015-02-23 11:32:49 +0300

    &yet

--- middleware/tracks.js
+++ middleware/tracks.js
@@ -2,27 +2,35 @@
 var faker;
 faker = require('faker');
 module.exports = function*(next){
-  var tracks, fakeduration, record, i$, i, id;
+  var tracks, types, fakeduration, record, i$, i, type, ext, id;
   tracks = [];
+  types = ['audio', 'video', 'audiomashup', 'videomashup'];
   fakeduration = function(){
     return '0' + faker.finance.mask() % 10 + ':' + faker.finance.mask() % 100;
   };
   record = function(){
     return {
       id: id,
+      type: type,
       duration: fakeduration(),
       title: faker.company.bs(),
       plays: faker.finance.mask(),
       poster: faker.image.image(),
       artist: faker.name.findName(),
       avatarurl: faker.internet.avatar(),
-      streamurl: '/stream/' + id + '.mp3',
-      downloadurl: '/download/' + id + '.mp3'
+      streamurl: '/stream/' + id + ext,
+      downloadurl: '/download/' + id + ext
     };
   };
-  for (i$ = 1; i$ <= 20; ++i$) {
+  for (i$ = 1; i$ <= 40; ++i$) {
     i = i$;
+    type = types.pop();
+    types.unshift(type);
+    ext = type === 'audio' || type === 'audiomashup' ? '.mp3' : '.mp4';
     id = faker.finance.mask() % 10;
+    if (ext === '.mp4') {
+      id = Math.floor(id / 2) + 1;
+    }
     if (id === 0) {
       id += 1;
     }
